543a9a863029c5ac521b219126eca6148deef3a2
jorge <jorge@LAPTOP-Q8O6J02O>
2025-03-25 15:56:45 -0600

    Borre una linea que no servia para nada

--- src/BaseDatos.java
+++ src/BaseDatos.java
@@ -2,6 +2,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -96,4 +97,5 @@ public class BaseDatos {
         }
         return bandera;
     }
+    
 }
