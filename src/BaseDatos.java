02e589edad0a03098a8fa09b6ecec13702b01227
jorge <jorge@LAPTOP-Q8O6J02O>
2025-03-25 01:19:59 -0600

    Completa la conexion

--- src/BaseDatos.java
+++ src/BaseDatos.java
@@ -46,4 +46,54 @@ public class BaseDatos {
             Logger.getLogger(BaseDatos.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
+    public boolean insertarLogin(String n,String cor,String c){
+        try {
+            String SQL="INSERT INTO `Gerente` (`Nombre`,`Correo`, `Contrasena`) VALUES ('%Nom%', '%COR%','%CON%');";
+            SQL=SQL.replaceAll("%Nom%",n);
+            SQL=SQL.replaceAll("%COR%",cor);
+            SQL=SQL.replaceAll("%CON%",c);
+            transaccion.execute(SQL);
+        } catch (SQLException ex) {return false;}
+        System.out.println("insertamos");
+        return true;
+    }  
+    public int getLogin(String correo, String contrasena){
+        String A[] = new String[2];
+        int idEmpleado = -1;
+        try {
+            //SELECT * FROM `Login` WHERE Correo like 'A' and Constrasena like 'A';
+            String SQL="SELECT * FROM `Login` WHERE Correo LIKE '"+correo+"' AND Contrasena LIKE '"+contrasena+"'";
+            cursor= transaccion.executeQuery(SQL);
+            if(cursor.next()){
+                idEmpleado = cursor.getInt(1);
+                A[0]=cursor.getString(2);
+                A[1]=cursor.getString(3);
+                if(A[0].equals(correo) && A[1].equals(contrasena)){
+                    idEmpleado = cursor.getInt(1);
+                };
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(BaseDatos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return idEmpleado;
+    }
+    public boolean buscarLogin(String correo, String contrasena){//validar login
+        String A[] = new String[2];
+        boolean bandera = false;
+        try {
+            String SQL="SELECT * FROM `Login` WHERE Correo LIKE '"+correo+"' AND Contrasena LIKE '"+contrasena+"'";
+            cursor= transaccion.executeQuery(SQL);
+            if(cursor.next()){
+                A[0]=cursor.getString(2);
+                A[1]=cursor.getString(3);
+                if(A[0].equals(correo) && A[1].equals(contrasena))bandera=true;
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(BaseDatos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return bandera;
+    }
 }
--- src/VentanaLogin.java
+++ src/VentanaLogin.java
@@ -39,21 +39,66 @@ public class VentanaLogin extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jButton1.setText("jButton1");
+
+        jLabel1.setText("Correo");
+
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setText("Contrasenia");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(144, 144, 144)
+                        .addComponent(jButton1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(46, 46, 46)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jTextField1, javax.swing.GroupLayout.DEFAULT_SIZE, 125, Short.MAX_VALUE)
+                            .addComponent(jTextField2)
+                            .addComponent(jLabel2))))
+                .addContainerGap(181, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(40, 40, 40)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 40, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(91, 91, 91))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -91,5 +136,10 @@ public class VentanaLogin extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
     // End of variables declaration//GEN-END:variables
 }
