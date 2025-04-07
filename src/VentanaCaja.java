a5b3126733934bba64a4362846135f48bb5a41e4
JorgeRoblesBuendia <joroblesbu@ittepic.edu.mx>
2025-04-07 08:39:15 -0600

    Arregle mas del menu del empleado 2 jorge

--- src/VentanaCaja.java
+++ src/VentanaCaja.java
@@ -93,7 +93,7 @@ public class VentanaCaja extends javax.swing.JFrame {
             }
         });
 
-        jButton1.setText("jButton1");
+        jButton1.setText("Regresar");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
